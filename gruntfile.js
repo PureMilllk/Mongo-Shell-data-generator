b2158edaec8c1d9056331c31a2255459c1661424
PureMilllk <puremilllk@gmail.com>
2015-11-10 11:46:20 +0800

    grunt setup

--- gruntfile.js
+++ gruntfile.js
@@ -1,50 +1,82 @@
-var fs = require('fs');
+/*global module:false*/
+module.exports = function(grunt) {
 
-module.exports = grunt => {
-	'use strict';
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['lib/<%= pkg.name %>.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        globals: {
+          jQuery: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
+    nodeunit: {
+      files: ['test/**/*_test.js']
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'nodeunit']
+      }
+    }
+  });
 
-	grunt.initConfig({
-		test: {
-			test: "Milk",
-			src: "js/test.js",
-			dest: "src/test.js"
-		},
-		multi: {
-			test1: {
-				test: 'test1'
-			},
-			test2: {
-				test: 'test2'
-			}
-		}
-	});
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'nodeunit', 'concat', 'uglify']);
 
-	grunt.registerTask('test', 'for test purpose', function () {
-
-		var callback = this.async();
-
-		grunt.config.requires('test.test');
-
-		grunt.log.writeln(`\n
-			grunt running \n\
-			${ grunt.config.get('test.test') }\n\
-			${ grunt.config.get('test.src') }\n\
-			${ grunt.config.get('test.dest') }\n\
-			`);
-			fs.readFile(grunt.config.get('test.src'), function (err, data) {
-				if (err) {
-					grunt.log.writeln(`error: ${err}`);
-				}
-				fs.writeFile(grunt.config.get('test.dest'), data);
-				callback(grunt.log.writeln('test done'));
-			})
-	});
-
-	grunt.registerTask('run', 'All tasks', ['test']);
-
-	grunt.registerMultiTask('multi', 'info', function () {
-		grunt.log.writeln(this.data.test);
-	});
-
-}
+};
